cd60dd2cdd4f8fa38ef84bf3a7fe3fe9cdebfb57
Gerastenok Konstantin <frankgover@rambler.ru>
2019-11-30 17:41:12 +0000

    Upload New File

---
+++ WinAMP/WMPDSPEqz.pas
@@ -0,0 +1,80 @@
+unit
+  WMPDSPEqz;
+
+interface
+
+uses
+  BQFilter,
+  WMPDSPPlug;
+
+type
+  TFilters = array[0..4, 0..18] of TBQFilter;
+
+type
+  TWMPDSPEqz = class(TWMPDSPPlug)
+  private
+    feqz: TFilters;
+  public
+    constructor Create(); override;
+    destructor Destroy(); override;
+    procedure Process(); override;
+  end;
+
+var
+  DSPEqz: TWMPDSPEqz;
+
+implementation
+
+uses
+  Math;
+
+constructor TWMPDSPEqz.Create();
+var
+  k: LongWord;
+  i: LongWord;
+begin
+  inherited Create();
+  for k := 0 to Length(self.feqz) - 1 do begin
+    for i := 0 to Length(self.feqz[k]) - 1 do begin
+      self.feqz[k, i] := TBQFilter.Create();
+    end;
+  end;
+end;
+
+destructor TWMPDSPEqz.Destroy();
+var
+  k: LongWord;
+  i: LongWord;
+begin
+  for k := 0 to Length(self.feqz) - 1 do begin
+    for i := 0 to Length(self.feqz[k]) - 1 do begin
+      self.feqz[k, i].Destroy();
+    end;
+  end;
+  inherited Destroy();
+end;
+
+procedure TWMPDSPEqz.Process();
+var
+  k: LongWord;
+  i: LongWord;
+  x: LongWord;
+begin
+  for k := 0 to self.Data.nch - 1 do begin
+    for i := 0 to Length(self.feqz[k]) - 1 do begin
+      self.feqz[k, i].Gain := Power(10, ((self.Info.preamp + self.Info.bands[i]) / 10) / 20);
+      self.feqz[k, i].Band := btOctave;
+      self.feqz[k, i].Filter := ftEqu;
+      self.feqz[k, i].Enabled := self.Info.enabled;
+      self.feqz[k, i].Width := 0.5;
+      self.feqz[k, i].Frequency := 35 * Power(2, self.feqz[k, i].Width * i);
+      self.feqz[k, i].SampleRate := self.Data.srate;
+      for x := 0 to self.Data.samples - 1 do begin
+        self.Samples[k, x] := self.feqz[k, i].Process(self.Samples[k, x]);
+      end;
+    end;
+  end;
+end;
+
+begin
+end.
