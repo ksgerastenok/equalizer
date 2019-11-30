3e15d3d31f3146f5f63e2b136a2481941c48a854
Gerastenok Konstantin <frankgover@rambler.ru>
2019-11-30 17:40:52 +0000

    Upload New File

---
+++ WinAMP/WMPDSPEqu.dpr
@@ -0,0 +1,54 @@
+library
+  WMPDSPEqu;
+
+uses
+  XPMan,
+  WMPDSPMod,
+  WMPDSPDecl;
+
+{$R *.res}
+
+function getModule(const which: Integer): PWMPDSPModule; cdecl;
+begin
+  Result := nil;
+  case (which) of
+    0: begin
+      New(Result);
+      if((not(Result = nil))) then begin
+        Result.instance := 0;
+        Result.description := 'SuperEqu';
+        Result.Init := equInit;
+        Result.Quit := equQuit;
+        Result.Config := equConfig;
+        Result.ModifySamples := equProcess;
+      end;
+    end;
+    else begin
+      if((not(Result = nil))) then begin
+        Result.instance := 0;
+        Result.description := nil;
+        Result.Init := nil;
+        Result.Quit := nil;
+        Result.Config := nil;
+        Result.ModifySamples := nil;
+      end;
+      Dispose(Result);
+    end;
+  end;
+end;
+
+function winampDSPGetHeader2(): PWMPDSPHeader; cdecl;
+begin
+  New(Result);
+  if((not(Result = nil))) then begin
+    Result.version := $20;
+    Result.description := 'Nullsoft Equalizer Plugin v3.51';
+    Result.getModule := getModule;
+  end;
+end;
+
+exports
+  winampDSPGetHeader2;
+
+begin
+end.
