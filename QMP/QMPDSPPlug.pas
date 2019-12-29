bf012dba810fd14baf2acfebd082dd29b509d246
Gerastenok Konstantin <frankgover@rambler.ru>
2019-12-29 09:19:46 +0000

    Replace QMPDSPPlug.pas

--- QMP/QMPDSPPlug.pas
+++ QMP/QMPDSPPlug.pas
@@ -12,16 +12,14 @@ type
     finfo: PEQInfo;
     fdata: PWriteData;
     function getInfo(): PEQInfo;
-    function getData(): PWriteData;
-    function getSamples(const Channel: LongWord; const Sample: LongWord): Double;
-    procedure setSamples(const Channel: LongWord; const Sample: LongWord; const Value: Double);
+    function getSamples(const Sample: LongWord; const Channel: LongWord): Double;
+    procedure setSamples(const Sample: LongWord; const Channel: LongWord; const Value: Double);
   public
     constructor Create(); virtual;
-    destructor Destroy(); virtual;
-    procedure Process(); virtual; abstract;
+    destructor Destroy(); override;
+    procedure Process(const Data: Pointer; const Samples: LongWord; const Bits: LongWord; const Channels: LongWord; const Rates: LongWord); virtual;
     property Info: PEQInfo read getInfo;
-    property Data: PWriteData read getData;
-    property Samples[const Channel: LongWord; const Sample: LongWord]: Double read getSamples write setSamples;
+    property Samples[const Sample: LongWord; const Channel: LongWord]: Double read getSamples write setSamples;
   end;
 
 implementation
@@ -40,76 +38,84 @@ begin
   inherited Destroy();
 end;
 
+procedure TQMPDSPPlug.Process(const Data: Pointer; const Samples: LongWord; const Bits: LongWord; const Channels: LongWord; const Rates: LongWord);
+begin
+  self.fdata.data := Data;
+  self.fdata.bits := Bits;
+  self.fdata.rates := Rates;
+  self.fdata.samples := Samples;
+  self.fdata.channels := Channels;
+end;
+
 function TQMPDSPPlug.getInfo(): PEQInfo;
 begin
   Result := self.finfo;
 end;
 
-function TQMPDSPPlug.getData(): PWriteData;
-begin
-  Result := self.fdata;
-end;
-
-function TQMPDSPPlug.getSamples(const Channel: LongWord; const Sample: LongWord): Double;
+function TQMPDSPPlug.getSamples(const Sample: LongWord; const Channel: LongWord): Double;
 var
   x: Double;
   p: Pointer;
 begin
-  p := Pointer(LongWord(self.fdata.data) + ((self.fdata.bps div 8) * (Channel + Sample * self.fdata.nch)));
-  case (self.fdata.bps div 8) of
-    1: begin
-      x := ShortInt(p^) / $0000007F;
-    end;
-    2: begin
-      x := SmallInt(p^) / $00007FFF;
-    end;
-    4: begin
-      x := LongInt(p^) / $7FFFFFFF;
-    end;
-    else begin
-      x := 0;
+  if((Sample < self.fdata.samples) and (Channel < self.fdata.channels)) then begin
+    p := Pointer(LongWord(self.fdata.data) + ((self.fdata.bits div 8) * (Channel + Sample * self.fdata.channels)));
+    case (self.fdata.bits div 8) of
+      1: begin
+        x := ShortInt(p^) / $0000007F;
+      end;
+      2: begin
+        x := SmallInt(p^) / $00007FFF;
+      end;
+      4: begin
+        x := LongInt(p^) / $7FFFFFFF;
+      end;
+      else begin
+        x := 0 / $00007FFF;
+      end;
     end;
   end;
-  if((x <= -1.0)) then begin
+  if((x < -1.0)) then begin
     x := -1.0;
   end;
-  if((x = 0)) then begin
+  if((x = 0.0)) then begin
     x := 0.0;
   end;
-  if((x >= +1.0)) then begin
+  if((x > +1.0)) then begin
     x := +1.0;
   end;
   Result := x;
 end;
 
-procedure TQMPDSPPlug.setSamples(const Channel: LongWord; const Sample: LongWord; const Value: Double);
+procedure TQMPDSPPlug.setSamples(const Sample: LongWord; const Channel: LongWord; const Value: Double);
 var
   x: Double;
   p: Pointer;
 begin
   x := Value;
-  if((x <= -1.0)) then begin
+  if((x < -1.0)) then begin
     x := -1.0;
   end;
-  if((x = 0)) then begin
+  if((x = 0.0)) then begin
     x := 0.0;
   end;
-  if((x >= +1.0)) then begin
+  if((x > +1.0)) then begin
     x := +1.0;
   end;
-  p := Pointer(LongWord(self.fdata.data) + ((self.fdata.bps div 8) * (Channel + Sample * self.fdata.nch)));
-  case (self.fdata.bps div 8) of
-    1: begin
-      ShortInt(p^) := Round(x * $0000007F);
-    end;
-    2: begin
-      SmallInt(p^) := Round(x * $00007FFF);
-    end;
-    4: begin
-      LongInt(p^) := Round(x * $7FFFFFFF);
-    end;
-    else begin
-      SmallInt(p^) := 0;
+  if((Sample < self.fdata.samples) and (Channel < self.fdata.channels)) then begin
+    p := Pointer(LongWord(self.fdata.data) + ((self.fdata.bits div 8) * (Channel + Sample * self.fdata.channels)));
+    case (self.fdata.bits div 8) of
+      1: begin
+        ShortInt(p^) := Round(x * $0000007F);
+      end;
+      2: begin
+        SmallInt(p^) := Round(x * $00007FFF);
+      end;
+      4: begin
+        LongInt(p^) := Round(x * $7FFFFFFF);
+      end;
+      else begin
+        SmallInt(p^) := Round(0 * $00007FFF);
+      end;
     end;
   end;
 end;
