23ca8a5619f14f3039267c0fc76313786b56abc5
Gerastenok Konstantin <frankgover@rambler.ru>
2019-12-29 09:19:07 +0000

    Replace QMPDSPMod.pas

--- QMP/QMPDSPMod.pas
+++ QMP/QMPDSPMod.pas
@@ -7,8 +7,8 @@ uses
   QMPDSPDecl;
 
 function equInit(const flags: Integer): Integer; cdecl;
-function equProcess(const writeData: PWriteData; const latency: PInteger; const flags: Integer): Integer; cdecl;
-function equEQUpdate(const EQInfo: PEQInfo; const flags: Integer): Integer; cdecl;
+function equProcess(const data: PWriteData; const latency: PInteger; const flags: Integer): Integer; cdecl;
+function equUpdate(const info: PEQInfo; const flags: Integer): Integer; cdecl;
 procedure equShutdown(const flags: Integer); cdecl;
 
 implementation
@@ -27,22 +27,17 @@ begin
   DSPEqz.Destroy();
 end;
 
-function equEQUpdate(const EQInfo: PEQInfo; const flags: Integer): Integer;
+function equUpdate(const info: PEQInfo; const flags: Integer): Integer;
 begin
-  DSPEqz.Info.bands := EQInfo.bands;
-  DSPEqz.Info.preamp := EQInfo.preamp;
-  DSPEqz.Info.enabled := EQInfo.enabled;
+  DSPEqz.Info.bands := info.bands;
+  DSPEqz.Info.preamp := info.preamp;
+  DSPEqz.Info.enabled := info.enabled;
   Result := 1;
 end;
 
-function equProcess(const writeData: PWriteData; const latency: PInteger; const flags: Integer): Integer;
+function equProcess(const data: PWriteData; const latency: PInteger; const flags: Integer): Integer;
 begin
-  DSPEqz.Data.bps := writeData.bps;
-  DSPEqz.Data.nch := writeData.nch;
-  DSPEqz.Data.data := writeData.data;
-  DSPEqz.Data.srate := writeData.srate;
-  DSPEqz.Data.samples := writeData.samples;
-  DSPEqz.Process();
+  DSPEqz.Process(data.data, data.samples, data.bits, data.channels, data.rates);
   Result := 1;
 end;
 
