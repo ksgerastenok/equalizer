3cb3889e872301c69d8c6a826218d471520b0daa
Gerastenok Konstantin <frankgover@rambler.ru>
2019-12-29 09:24:56 +0000

    Replace WMPDSPMod.pas

--- WinAMP/WMPDSPMod.pas
+++ WinAMP/WMPDSPMod.pas
@@ -8,7 +8,7 @@ uses
 
 function equInit(const tmod: PWMPDSPModule): Integer; cdecl;
 procedure equQuit(const tmod: PWMPDSPModule); cdecl;
-function equProcess(const tmod: PWMPDSPModule; const data: Pointer; const samples, bps, nch, srate: Integer): Integer; cdecl;
+function equProcess(const tmod: PWMPDSPModule; const data: Pointer; const samples: Integer; const bits: Integer; const channels: Integer; const rates: Integer): Integer; cdecl;
 procedure equConfig(const tmod: PWMPDSPModule); cdecl;
 
 implementation
@@ -18,45 +18,15 @@ uses
   WMPDSPForm;
 
 function equInit(const tmod: PWMPDSPModule): Integer;
-var
-  f: file;
-  i: Integer;
 begin
   DSPEqz := TWMPDSPEqz.Create();
-  try
-    System.Assign(f, 'equalizer.cfg');
-    System.ReSet(f, 1);
-    System.BlockRead(f, DSPEqz.Info^, SizeOf(TEQInfo) * 1);
-    System.Close(f);
-  except
-    DSPEqz.Info.preamp := 0;
-    DSPEqz.Info.enabled := false;
-    for i := 0 to Length(DSPEqz.Info.bands) - 1 do begin
-      DSPEqz.Info.bands[i] := 0;
-    end;
-  end;
-  CFGForm := TWMPDSPForm.CreateParented(0);
+  CFGForm := TWMPDSPForm.Create(nil);
   Result := 0;
 end;
 
 procedure equQuit(const tmod: PWMPDSPModule);
-var
-  f: file;
-  i: Integer;
 begin
   CFGForm.Destroy();
-  try
-    System.Assign(f, 'equalizer.cfg');
-    System.ReWrite(f, 1);
-    System.BlockWrite(f, DSPEqz.Info^, SizeOf(TEQInfo) * 1);
-    System.Close(f);
-  except
-    DSPEqz.Info.preamp := 0;
-    DSPEqz.Info.enabled := false;
-    for i := 0 to Length(DSPEqz.Info.bands) - 1 do begin
-      DSPEqz.Info.bands[i] := 0;
-    end;
-  end;
   DSPEqz.Destroy();
 end;
 
@@ -65,14 +35,9 @@ begin
   CFGForm.Show();
 end;
 
-function equProcess(const tmod: PWMPDSPModule; const data: Pointer; const samples, bps, nch, srate: Integer): Integer;
+function equProcess(const tmod: PWMPDSPModule; const data: Pointer; const samples: Integer; const bits: Integer; const channels: Integer; const rates: Integer): Integer;
 begin
-  DSPEqz.Data.bps := bps;
-  DSPEqz.Data.nch := nch;
-  DSPEqz.Data.data := data;
-  DSPEqz.Data.srate := srate;
-  DSPEqz.Data.samples := samples;
-  DSPEqz.Process();
+  DSPEqz.Process(data, samples, bits, channels, rates);
   Result := samples;
 end;
 
