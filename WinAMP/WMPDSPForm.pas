b791c14b25e1d106a7680d23b69b67b9741933d7
Gerastenok Konstantin <frankgover@rambler.ru>
2020-07-24 21:32:37 +0000

    Replace WMPDSPForm.pas

--- WinAMP/WMPDSPForm.pas
+++ WinAMP/WMPDSPForm.pas
@@ -58,9 +58,10 @@ type
     stLowValue: TStaticText;
     stZeroValue: TStaticText;
     stHighValue: TStaticText;
+    constructor Create(); virtual;
+    destructor Destroy(); override;
     procedure FormMainProcess(const Sender: TObject);
-    procedure CheckBoxProcess(const Sender: TObject);
-    procedure TrackBarProcess(const Sender: TObject);
+    procedure ControlsProcess(const Sender: TObject);
   end;
 
 var
@@ -73,53 +74,67 @@ uses
 
 {$R *.dfm}
 
-procedure TWMPDSPForm.FormMainProcess(const Sender: TObject);
+constructor TWMPDSPForm.Create();
 begin
-  self.ActiveControl := nil;
-  self.tbPreamp.Position := self.tbPreamp.Max - DSPEqz.Info.preamp + self.tbPreamp.Min;
-  self.tbBand00.Position := self.tbBand00.Max - DSPEqz.Info.bands[00] + self.tbBand00.Min;
-  self.tbBand01.Position := self.tbBand01.Max - DSPEqz.Info.bands[01] + self.tbBand01.Min;
-  self.tbBand02.Position := self.tbBand02.Max - DSPEqz.Info.bands[02] + self.tbBand02.Min;
-  self.tbBand03.Position := self.tbBand03.Max - DSPEqz.Info.bands[03] + self.tbBand03.Min;
-  self.tbBand04.Position := self.tbBand04.Max - DSPEqz.Info.bands[04] + self.tbBand04.Min;
-  self.tbBand05.Position := self.tbBand05.Max - DSPEqz.Info.bands[05] + self.tbBand05.Min;
-  self.tbBand06.Position := self.tbBand06.Max - DSPEqz.Info.bands[06] + self.tbBand06.Min;
-  self.tbBand07.Position := self.tbBand07.Max - DSPEqz.Info.bands[07] + self.tbBand07.Min;
-  self.tbBand08.Position := self.tbBand08.Max - DSPEqz.Info.bands[08] + self.tbBand08.Min;
-  self.tbBand09.Position := self.tbBand09.Max - DSPEqz.Info.bands[09] + self.tbBand09.Min;
-  self.tbBand10.Position := self.tbBand10.Max - DSPEqz.Info.bands[10] + self.tbBand10.Min;
-  self.tbBand11.Position := self.tbBand11.Max - DSPEqz.Info.bands[11] + self.tbBand11.Min;
-  self.tbBand12.Position := self.tbBand12.Max - DSPEqz.Info.bands[12] + self.tbBand12.Min;
-  self.tbBand13.Position := self.tbBand13.Max - DSPEqz.Info.bands[13] + self.tbBand13.Min;
-  self.tbBand14.Position := self.tbBand14.Max - DSPEqz.Info.bands[14] + self.tbBand14.Min;
-  self.tbBand15.Position := self.tbBand15.Max - DSPEqz.Info.bands[15] + self.tbBand15.Min;
-  self.tbBand16.Position := self.tbBand16.Max - DSPEqz.Info.bands[16] + self.tbBand16.Min;
-  self.tbBand17.Position := self.tbBand17.Max - DSPEqz.Info.bands[17] + self.tbBand17.Min;
-  self.tbBand18.Position := self.tbBand18.Max - DSPEqz.Info.bands[18] + self.tbBand18.Min;
-  self.cbEnabled.Checked := DSPEqz.Info.enabled;
+  inherited Create(nil);
 end;
 
-procedure TWMPDSPForm.CheckBoxProcess(const Sender: TObject);
+destructor TWMPDSPForm.Destroy();
+begin
+  inherited Destroy();
+end;
+
+procedure TWMPDSPForm.FormMainProcess(const Sender: TObject);
+var
+  i: Integer;
 begin
   self.ActiveControl := nil;
-  if(((Sender as TCheckBox).Tag = 99)) then begin
-    DSPEqz.Info.enabled := (Sender as TCheckBox).Checked;
-    (Sender as TCheckBox).Hint := Format('Enabled: %s', [BoolToStr(DSPEqz.Info.enabled, True)]);
-  end                                  else begin
-    DSPEqz.Info.enabled := (Sender as TCheckBox).Checked;
-    (Sender as TCheckBox).Hint := Format('Enabled: %s', [BoolToStr(DSPEqz.Info.enabled, True)]);
+  for i := 0 to self.ControlCount - 1 do begin
+    if((self.Controls[i] = self.Controls[i])) then begin
+      if((self.Controls[i] is TCheckBox)) then begin
+        if(((self.Controls[i] as TCheckBox).Tag = 99)) then begin
+          (self.Controls[i] as TCheckBox).Checked := DSPEqz.Info.enabled;
+        end                               else begin
+          (self.Controls[i] as TCheckBox).Checked := DSPEqz.Info.enabled;
+        end;
+        (self.Controls[i] as TCheckBox).Hint := Format('Enabled: %s', [BoolToStr((self.Controls[i] as TCheckBox).Checked, True)]);
+      end;
+      if((self.Controls[i] is TTrackBar)) then begin
+        if(((self.Controls[i] as TTrackBar).Tag = 99)) then begin
+          (self.Controls[i] as TTrackBar).Position := (self.Controls[i] as TTrackBar).Max - DSPEqz.Info.preamp                                     + (self.Controls[i] as TTrackBar).Min;
+        end                               else begin
+          (self.Controls[i] as TTrackBar).Position := (self.Controls[i] as TTrackBar).Max - DSPEqz.Info.bands[(self.Controls[i] as TTrackBar).Tag] + (self.Controls[i] as TTrackBar).Min;
+        end;
+        (self.Controls[i] as TTrackBar).Hint := Format('Gain: %s dB', [FloatToStr(((self.Controls[i] as TTrackBar).Max - (self.Controls[i] as TTrackBar).Position + (self.Controls[i] as TTrackBar).Min) / 10)]);
+      end;
+    end;
   end;
 end;
 
-procedure TWMPDSPForm.TrackBarProcess(const Sender: TObject);
+procedure TWMPDSPForm.ControlsProcess(const Sender: TObject);
+var
+  i: Integer;
 begin
   self.ActiveControl := nil;
-  if(((Sender as TTrackBar).Tag = 99)) then begin
-    DSPEqz.Info.preamp := (Sender as TTrackBar).Max - (Sender as TTrackBar).Position + (Sender as TTrackBar).Min;
-    (Sender as TTrackBar).Hint := Format('Gain: %s dB', [FloatToStr(DSPEqz.Info.preamp / 10)]);
-  end                                  else begin
-    DSPEqz.Info.bands[(Sender as TTrackBar).Tag] := (Sender as TTrackBar).Max - (Sender as TTrackBar).Position + (Sender as TTrackBar).Min;
-    (Sender as TTrackBar).Hint := Format('Gain: %s dB', [FloatToStr(DSPEqz.Info.bands[(Sender as TTrackBar).Tag] / 10)]);
+  for i := 0 to self.ControlCount - 1 do begin
+    if((self.Controls[i] = Sender)) then begin
+      if((self.Controls[i] is TCheckBox)) then begin
+        if(((self.Controls[i] as TCheckBox).Tag = 99)) then begin
+          DSPEqz.Info.enabled := (self.Controls[i] as TCheckBox).Checked;
+        end                               else begin
+          DSPEqz.Info.enabled := (self.Controls[i] as TCheckBox).Checked;
+        end;
+        (self.Controls[i] as TCheckBox).Hint := Format('Enabled: %s', [BoolToStr((self.Controls[i] as TCheckBox).Checked, True)]);
+      end;
+      if((self.Controls[i] is TTrackBar)) then begin
+        if(((self.Controls[i] as TTrackBar).Tag = 99)) then begin
+          DSPEqz.Info.preamp                                     := (self.Controls[i] as TTrackBar).Max - (self.Controls[i] as TTrackBar).Position + (self.Controls[i] as TTrackBar).Min;
+        end                               else begin
+          DSPEqz.Info.bands[(self.Controls[i] as TTrackBar).Tag] := (self.Controls[i] as TTrackBar).Max - (self.Controls[i] as TTrackBar).Position + (self.Controls[i] as TTrackBar).Min;
+        end;
+        (self.Controls[i] as TTrackBar).Hint := Format('Gain: %s dB', [FloatToStr(((self.Controls[i] as TTrackBar).Max - (self.Controls[i] as TTrackBar).Position + (self.Controls[i] as TTrackBar).Min) / 10)]);
+      end;
+    end;
   end;
 end;
 
