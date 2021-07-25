112d87eccf623fdfb200bdd024c262604b2f9ba9
Konstantin Gerastenok <frankgover@rambler.ru>
2021-07-25 23:19:18 +0300

    Add files via upload

---
+++ QMP/QMPDSP.pas
@@ -0,0 +1,131 @@
+unit
+  QMPDSP;
+
+interface
+
+uses
+  QMPDCL;
+
+type
+  TQMPDSP = class(TObject)
+  private
+    var fdata: PData;
+    var finfo: PInfo;
+    function getData(): PData;
+    function getInfo(): PInfo;
+    function getSamples(const Sample: Integer; const Channel: Integer): Double;
+    procedure setSamples(const Sample: Integer; const Channel: Integer; const Value: Double);
+  public
+    constructor Create(); virtual;
+    destructor Destroy(); override;
+    property Data: PData read getData;
+    property Info: PInfo read getInfo;
+    property Samples[const Sample: Integer; const Channel: Integer]: Double read getSamples write setSamples;
+  end;
+
+implementation
+
+constructor TQMPDSP.Create();
+begin
+  self.fdata := New(PData);
+  self.finfo := New(PInfo);
+end;
+
+destructor TQMPDSP.Destroy();
+begin
+  Dispose(self.fdata);
+  Dispose(self.finfo);
+end;
+
+function TQMPDSP.getData(): PData;
+begin
+  Result := self.fdata;
+end;
+
+function TQMPDSP.getInfo(): PInfo;
+begin
+  Result := self.finfo;
+end;
+
+function TQMPDSP.getSamples(const Sample: Integer; const Channel: Integer): Double;
+var
+  x: Double;
+  p: Pointer;
+begin
+  if((Sample < self.Data.Samples) and (Channel < self.Data.Channels)) then begin
+    p := Pointer(Integer(self.Data.Data) + ((self.Data.Bits div 8) * (Channel + Sample * self.Data.Channels)));
+  end                                                                 else begin
+    p := nil;
+  end;
+  try
+    case (self.Data.Bits div 8) of
+      1: begin
+        x := ShortInt(p^) / $0000007F;
+      end;
+      2: begin
+        x := SmallInt(p^) / $00007FFF;
+      end;
+      4: begin
+        x := LongInt(p^) / $7FFFFFFF;
+      end;
+      else begin
+        x := 0.0;
+      end;
+    end;
+  except
+    x := 0.0;
+  end;
+  if((x < -1.0)) then begin
+    x := -1.0;
+  end;
+  if((x = 0.0)) then begin
+    x := 0.0;
+  end;
+  if((x > +1.0)) then begin
+    x := +1.0;
+  end;
+  Result := x;
+end;
+
+procedure TQMPDSP.setSamples(const Sample: Integer; const Channel: Integer; const Value: Double);
+var
+  x: Double;
+  p: Pointer;
+begin
+  x := Value;
+  if((x < -1.0)) then begin
+    x := -1.0;
+  end;
+  if((x = 0.0)) then begin
+    x := 0.0;
+  end;
+  if((x > +1.0)) then begin
+    x := +1.0;
+  end;
+  if((Sample < self.Data.Samples) and (Channel < self.Data.Channels)) then begin
+    p := Pointer(Integer(self.Data.Data) + ((self.Data.Bits div 8) * (Channel + Sample * self.Data.Channels)));
+  end                                                                 else begin
+    p := nil;
+  end;
+  try
+    case (self.Data.Bits div 8) of
+      1: begin
+        ShortInt(p^) := Round(x * $0000007F);
+      end;
+      2: begin
+        SmallInt(p^) := Round(x * $00007FFF);
+      end;
+      4: begin
+        LongInt(p^) := Round(x * $7FFFFFFF);
+      end;
+      else begin
+        SmallInt(p^) := Round(x * $00000000);
+      end;
+    end;
+  except
+    SmallInt(p^) := Round(x * $00000000);
+  end;
+end;
+
+begin
+end.
---
+++ QMP/QMPEQU.pas
@@ -0,0 +1,123 @@
+unit
+  QMPEQU;
+
+interface
+
+uses
+  QMPBQF,
+  QMPDSP,
+  QMPDCL;
+
+type
+  TQMPEQU = record
+  private
+    class var fdsp: TQMPDSP;
+    class var feqz: array[0..4, 0..9] of TQMPBQF;
+    class function getDSP(): TQMPDSP; cdecl; static;
+    class procedure Initialize(); cdecl; static;
+    class procedure Finalize(); cdecl; static;
+    class function Init(const Flags: Integer): Integer; cdecl; static;
+    class function Modify(const Data: PData; const Latency: Pointer; const Flags: Integer): Integer; cdecl; static;
+    class function Update(const Info: PInfo; const Flags: Integer): Integer; cdecl; static;
+    class procedure Quit(const Flags: Integer); cdecl; static;
+  public
+    class function Plugin(): PPlugin; cdecl; static;
+    class property DSP: TQMPDSP read getDSP;
+  end;
+
+implementation
+
+uses
+  Math;
+
+class function TQMPEQU.Plugin(): PPlugin; cdecl;
+begin
+  Result := New(PPlugin);
+  Result.Description := 'Quinnware Equalizer v3.51';
+  Result.Version := $0000;
+  Result.Init := TQMPEQU.Init;
+  Result.Quit := TQMPEQU.Quit;
+  Result.Modify := TQMPEQU.Modify;
+  Result.Update := TQMPEQU.Update;
+end;
+
+class function TQMPEQU.getDSP(): TQMPDSP; cdecl;
+begin
+  Result := TQMPEQU.fdsp;
+end;
+
+class procedure TQMPEQU.Initialize(); cdecl;
+var
+  k: Integer;
+  i: Integer;
+begin
+  TQMPEQU.fdsp := TQMPDSP.Create();
+  for k := 0 to Length(TQMPEQU.feqz) - 1 do begin
+    for i := 0 to Length(TQMPEQU.feqz[k]) - 1 do begin
+      TQMPEQU.feqz[k, i] := TQMPBQF.Create(ftEqu, btOctave, gtDb);
+    end;
+  end;
+end;
+
+class procedure TQMPEQU.Finalize(); cdecl;
+var
+  k: Integer;
+  i: Integer;
+begin
+  for k := 0 to Length(TQMPEQU.feqz) - 1 do begin
+    for i := 0 to Length(TQMPEQU.feqz[k]) - 1 do begin
+      TQMPEQU.feqz[k, i].Destroy();
+    end;
+  end;
+  TQMPEQU.fdsp.Destroy();
+end;
+
+class function TQMPEQU.Init(const Flags: Integer): Integer; cdecl;
+begin
+  TQMPEQU.Initialize();
+  Result := 1;
+end;
+
+class procedure TQMPEQU.Quit(const Flags: Integer); cdecl;
+begin
+  TQMPEQU.Finalize();
+end;
+
+class function TQMPEQU.Modify(const Data: PData; const Latency: Pointer; const Flags: Integer): Integer; cdecl;
+var
+  k: Integer;
+  i: Integer;
+  x: Integer;
+begin
+  if((TQMPEQU.DSP.Info.Enabled)) then begin
+    TQMPEQU.DSP.Data.Data := Data.Data;
+    TQMPEQU.DSP.Data.Bits := Data.Bits;
+    TQMPEQU.DSP.Data.Rates := Data.Rates;
+    TQMPEQU.DSP.Data.Samples := Data.Samples;
+    TQMPEQU.DSP.Data.Channels := Data.Channels;
+    for k := 0 to TQMPEQU.DSP.Data.Channels - 1 do begin
+      for i := 0 to Length(TQMPEQU.DSP.Info.Bands) - 1 do begin
+        TQMPEQU.feqz[k, i].Amp := (TQMPEQU.DSP.Info.Preamp + TQMPEQU.DSP.Info.Bands[i]) / 10;
+        TQMPEQU.feqz[k, i].Freq := 35 * Power(2, 1.0 * i);
+        TQMPEQU.feqz[k, i].Rate := TQMPEQU.DSP.Data.Rates;
+        TQMPEQU.feqz[k, i].Width := 1.0;
+        for x := 0 to TQMPEQU.DSP.Data.Samples - 1 do begin
+          TQMPEQU.DSP.Samples[x, k] := TQMPEQU.feqz[k, i].Process(TQMPEQU.DSP.Samples[x, k]);
+        end;
+      end;
+    end;
+  end;
+  Result := 1;
+end;
+
+class function TQMPEQU.Update(const Info: PInfo; const Flags: Integer): Integer; cdecl;
+begin
+  TQMPEQU.DSP.Info.Size := Info.Size;
+  TQMPEQU.DSP.Info.Bands := Info.Bands;
+  TQMPEQU.DSP.Info.Preamp := Info.Preamp;
+  TQMPEQU.DSP.Info.Enabled := Info.Enabled;
+  Result := 1;
+end;
+
+begin
+end.
