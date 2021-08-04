b2231ca5ef74f802b4cdad7a6a6d286c1197b8c8
Konstantin Gerastenok <frankgover@rambler.ru>
2021-08-04 20:06:27 +0300

    Add files via upload

---
+++ WMP/WMPPLG.pas
@@ -0,0 +1,32 @@
+library
+  WMPPLG;
+
+uses
+  WMPDCL,
+  WMPEQU;
+
+function Plugin(const Which: Integer): PPlugin; cdecl;
+begin
+  case (Which) of
+    0: begin
+      Result := TWMPEQU.Plugin();
+    end;
+    else begin
+      Result := nil;
+    end;
+  end;
+end;
+
+function winampDSPGetHeader2(): PModule; cdecl;
+begin
+  Result := New(PModule);
+  Result^.Description := 'Nullsoft Equalizer Plugin v3.51';
+  Result^.Version := $0020;
+  Result^.Plugin := @Plugin;
+end;
+
+exports
+  winampDSPGetHeader2;
+
+begin
+end.
