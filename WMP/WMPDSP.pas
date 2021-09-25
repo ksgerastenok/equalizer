59cb81ef926608112b8b820e5b21656d3358541c
Konstantin Gerastenok <frankgover@rambler.ru>
2021-09-25 20:10:04 +0300

    Add files via upload

---
+++ WMP/WMPDSP.pas
@@ -0,0 +1,94 @@
+unit
+  WMPDSP;
+
+interface
+
+uses
+  WMPDCL;
+
+type
+  PWMPDSP = ^TWMPDSP;
+  TWMPDSP = record
+  private
+    var fdata: PData;
+    function clip(const Value: Double): Double;
+    function getSamples(const Sample: LongWord; const Channel: LongWord): Double;
+    procedure setSamples(const Sample: LongWord; const Channel: LongWord; const Value: Double);
+  public
+    procedure Init(const Data: PData);
+    procedure Done();
+    property Samples[const Sample: LongWord; const Channel: LongWord]: Double read getSamples write setSamples;
+  end;
+
+implementation
+
+procedure TWMPDSP.Init(const Data: PData);
+begin
+  self.fdata := Data;
+end;
+
+procedure TWMPDSP.Done();
+begin
+  self.fdata := nil;
+end;
+
+function TWMPDSP.clip(const Value: Double): Double;
+begin
+  Result := Value;
+  if((Result < -1.0)) then begin
+    Result := -1.0;
+  end;
+  if((Result = 0.0)) then begin
+    Result := 0.0;
+  end;
+  if((Result > +1.0)) then begin
+    Result := +1.0;
+  end;
+end;
+
+function TWMPDSP.getSamples(const Sample: LongWord; const Channel: LongWord): Double;
+begin
+  try
+    case (self.fdata.Bits div 8) of
+      1: begin
+        Result := clip(PShortInt(self.fdata.Data)[Channel + Sample * self.fdata.Channels] / $0000007F);
+      end;
+      2: begin
+        Result := clip(PSmallInt(self.fdata.Data)[Channel + Sample * self.fdata.Channels] / $00007FFF);
+      end;
+      4: begin
+        Result := clip(PLongInt(self.fdata.Data)[Channel + Sample * self.fdata.Channels] / $7FFFFFFF);
+      end;
+      else begin
+        Result := clip(0.0);
+      end;
+    end;
+  except
+    Result := clip(0.0);
+  end;
+end;
+
+procedure TWMPDSP.setSamples(const Sample: LongWord; const Channel: LongWord; const Value: Double);
+begin
+  try
+    case (self.fdata.Bits div 8) of
+      1: begin
+        PShortInt(self.fdata.Data)[Channel + Sample * self.fdata.Channels] := Round(clip(Value) * $0000007F);
+      end;
+      2: begin
+        PSmallInt(self.fdata.Data)[Channel + Sample * self.fdata.Channels] := Round(clip(Value) * $00007FFF);
+      end;
+      4: begin
+        PLongInt(self.fdata.Data)[Channel + Sample * self.fdata.Channels] := Round(clip(Value) * $7FFFFFFF);
+      end;
+      else begin
+        PSmallInt(self.fdata.Data)[Channel + Sample * self.fdata.Channels] := Round(clip(Value) * $00000000);
+      end;
+    end;
+  except
+    PSmallInt(self.fdata.Data)[Channel + Sample * self.fdata.Channels] := Round(clip(Value) * $00000000);
+  end;
+end;
+
+begin
+end.
