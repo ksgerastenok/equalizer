44c9390d0dbee373670b07e9e8746783af326366
Konstantin Gerastenok <frankgover@rambler.ru>
2022-12-04 17:39:45 +0300

    Delete CPP/QMP directory

--- CPP/QMP/QMPPLG.cpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "QMPDCL.h"
-#include "QMPMOD.h"
-#include "QMPPLG.h"
-
-EXPORT PMODULE QDSPModule() {
-    return QMPMOD::module();
-};
--- CPP/QMP/QMPPLG.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "QMPDCL.h"
-#define EXPORT extern "C" __declspec(dllexport)
-
-EXPORT PMODULE QDSPModule();
