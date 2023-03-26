d0badac4b81b0e6dfdc700e02e4339fcada60383
Konstantin Gerastenok <frankgover@rambler.ru>
2023-03-26 14:22:07 +0300

    Delete CPP directory

--- CPP/qmpplg.cpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "qmpdcl.h"
-#include "qmpplg.h"
-#include "qmpmod.h"
-#include "windows.h"
-
-EXPORT PMODULE QDSPModule() {
-    return QMPMOD::module();
-};
--- CPP/qmpplg.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "qmpdcl.h"
-#include "windows.h"
-#define EXPORT extern "C" __declspec(dllexport)
-
-EXPORT PMODULE QDSPModule();
