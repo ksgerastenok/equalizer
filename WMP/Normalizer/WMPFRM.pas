5e165e2f89e6d674c838aae1e5d8f8f01583ebc8
Konstantin Gerastenok <frankgover@rambler.ru>
2024-08-13 00:39:33 +0300

    Update WMPFRM.pas

--- WMP/Normalizer/WMPFRM.pas
+++ WMP/Normalizer/WMPFRM.pas
@@ -14,12 +14,8 @@ type
   PWMPFRM = ^TWMPFRM;
   TWMPFRM = record
   private
-    var finfo: TInfo;
-    var fform: TForm;
+    function getForm(): TForm;
     procedure setAmp(const Value: Double);
-    procedure CreateForm(const Top, Left, Width, Height: Integer; const Caption: String);
-    procedure CreateTrackBar(const Top, Left, Width, Height: Integer; const Tag: Integer);
-    procedure CreateStaticText(const Top, Left, Width, Height: Integer; const Caption: String);
     procedure Create();
     procedure Destroy();
   public
@@ -34,7 +30,6 @@ implementation
 
 uses
   Math,
-  StrUtils,
   SysUtils,
   Interfaces;
 
@@ -52,84 +47,113 @@ end;
 
 procedure TWMPFRM.Show();
 begin
-  self.fform.Show();
+  self.getForm().Show();
 end;
 
 procedure TWMPFRM.Hide();
 begin
-  self.fform.Hide();
+  self.getForm().Hide();
+end;
+
+function TWMPFRM.getForm(): TForm;
+var
+  i: Integer;
+begin
+  for i := 0 to Application.ComponentCount - 1 do begin
+    if (Application.Components[i] is TForm) then begin
+      Result := (Application.Components[i] as TForm);
+    end;
+  end;
 end;
 
 procedure TWMPFRM.Create();
 begin
-  self.CreateForm(120, 215, 450, 45, 'Normalizer');
-  self.CreateTrackBar(0, 0, 450, 20, 0);
-  self.CreateStaticText(25, 0, 45, 20, '0 dB');
-  self.CreateStaticText(25, 110, 45, 20, '5 dB');
-  self.CreateStaticText(25, 215, 45, 20, '10 dB');
-  self.CreateStaticText(25, 320, 45, 20, '15 dB');
-  self.CreateStaticText(25, 420, 45, 20, '20 dB');
+  with (TForm.Create(Application)) do begin
+    Font.Size := 8;
+    Caption := 'Normalizer';
+    Top := 120;
+    Left := 215;
+    Width := 450;
+    Height := 45;
+    BorderIcons := [biSystemMenu];
+    BorderStyle := bsSingle;
+    Position := poMainFormCenter;
+    ShowHint := True;
+    Parent := nil;
+  end;
+  with (TTrackBar.Create(Application)) do begin
+    Font.Size := 8;
+    Orientation := trHorizontal;
+    Tag := 0;
+    Top := 0;
+    Left := 0;
+    Width := 450;
+    Height := 20;
+    Min := 0;
+    Position := 0;
+    Max := 200;
+    Enabled := False;
+    ShowHint := True;
+    TickMarks := tmBoth;
+    TickStyle := tsNone;
+    Parent := self.getForm();
+  end;
+  with (TStaticText.Create(Application)) do begin
+    Font.Size := 8;
+    Caption := '0 dB';
+    Top := 25;
+    Left := 0;
+    Width := 45;
+    Height := 20;
+    ShowHint := True;
+    Parent := self.getForm();
+  end;
+  with (TStaticText.Create(Application)) do begin
+    Font.Size := 8;
+    Caption := '5 dB';
+    Top := 25;
+    Left := 110;
+    Width := 45;
+    Height := 20;
+    ShowHint := True;
+    Parent := self.getForm();
+  end;
+  with (TStaticText.Create(Application)) do begin
+    Font.Size := 8;
+    Caption := '10 dB';
+    Top := 25;
+    Left := 215;
+    Width := 45;
+    Height := 20;
+    ShowHint := True;
+    Parent := self.getForm();
+  end;
+  with (TStaticText.Create(Application)) do begin
+    Font.Size := 8;
+    Caption := '15 dB';
+    Top := 25;
+    Left := 320;
+    Width := 45;
+    Height := 20;
+    ShowHint := True;
+    Parent := self.getForm();
+  end;
+  with (TStaticText.Create(Application)) do begin
+    Font.Size := 8;
+    Caption := '20 dB';
+    Top := 25;
+    Left := 420;
+    Width := 45;
+    Height := 20;
+    ShowHint := True;
+    Parent := self.getForm();
+  end;
 end;
 
 procedure TWMPFRM.Destroy();
 begin
-  self.fform.Close();
-  self.fform.Destroy();
-end;
-
-procedure TWMPFRM.CreateForm(const Top, Left, Width, Height: Integer; const Caption: String);
-var
-  Result: TForm;
-begin
-  Result := TForm.Create(nil);
-  Result.Font.Size := 8;
-  Result.Caption := Caption;
-  Result.Top := Top;
-  Result.Left := Left;
-  Result.Width := Width;
-  Result.Height := Height;
-  Result.BorderIcons := [biSystemMenu];
-  Result.BorderStyle := bsSingle;
-  Result.Position := poMainFormCenter;
-  Result.ShowHint := True;
-  self.fform := Result;
-end;
-
-procedure TWMPFRM.CreateTrackBar(const Top, Left, Width, Height: Integer; const Tag: Integer);
-var
-  Result: TTrackBar;
-begin
-  Result := TTrackBar.Create(nil);
-  Result.Font.Size := 8;
-  Result.Orientation := trHorizontal;
-  Result.Tag := Tag;
-  Result.Top := Top;
-  Result.Left := Left;
-  Result.Width := Width;
-  Result.Height := Height;
-  Result.Min := 0;
-  Result.Position := 0;
-  Result.Max := 200;
-  Result.Enabled := False;
-  Result.ShowHint := True;
-  Result.TickMarks := tmBoth;
-  Result.TickStyle := tsNone;
-  Result.Parent := self.fform;
-end;
-
-procedure TWMPFRM.CreateStaticText(const Top, Left, Width, Height: Integer; const Caption: String);
-var
-  Result: TStaticText;
-begin
-  Result := TStaticText.Create(nil);
-  Result.Font.Size := 8;
-  Result.Caption := Caption;
-  Result.Top := Top;
-  Result.Left := Left;
-  Result.Width := Width;
-  Result.Height := Height;
-  Result.ShowHint := True;
-  Result.Parent := self.fform;
+  self.getForm().Close();
+  self.getForm().Destroy();
 end;
 
 procedure TWMPFRM.setAmp(const Value: Double);
@@ -137,10 +161,11 @@ var
   s: TTrackBar;
   i: Integer;
 begin
-  for i := 0 to self.fform.ControlCount - 1 do begin
-    if (self.fform.Controls[i] is TTrackBar) then begin
-      s := (self.fform.Controls[i] as TTrackBar);
-      s.Position := Round(20 * Log10(Value) * 10);
+  for i := 0 to self.getForm().ControlCount - 1 do begin
+    if (self.getForm().Controls[i] is TTrackBar) then begin
+      with(self.getForm().Controls[i] as TTrackBar) do begin
+        Position := Round(20 * Log10(Value) * 10);
+      end;
     end;
   end;
 end;
