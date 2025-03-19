43d52dafb7887382591eb594bddbabeee9573dae
Konstantin Gerastenok <frankgover@rambler.ru>
2025-03-19 22:41:27 +0300

    Add files via upload

---
+++ WMP/Enhancer/WMPENH.pas
@@ -0,0 +1,100 @@
+unit
+  WMPENH;
+
+interface
+
+uses
+  WMPDCL,
+  WMPDSP,
+  WMPBQF,
+  WMPFRM;
+
+type
+  PWMPENH = ^TWMPENH;
+  TWMPENH = record
+  private
+    class var ffrm: TWMPFRM;
+    class var fdsp: TWMPDSP;
+    class var fbss: array[0..4] of TWMPBQF;
+    class var ftrb: array[0..4] of TWMPBQF;
+    class function Init(const Module: PPlugin): Integer; cdecl; static;
+    class procedure Quit(const Module: PPlugin); cdecl; static;
+    class function Modify(const Module: PPlugin; const Data: Pointer; const Samples: LongWord; const Bits: LongWord; const Channels: LongWord; const Rates: LongWord): Integer; cdecl; static;
+    class procedure Config(const Module: PPlugin); cdecl; static;
+  public
+    class function Plugin(): PPlugin; cdecl; static;
+  end;
+
+implementation
+
+class function TWMPENH.Plugin(): PPlugin; cdecl;
+begin
+  Result := New(PPlugin);
+  Result.Description := 'Nullsoft Enhancer v3.51';
+  Result.Instance := $0000;
+  Result.Init := TWMPENH.Init;
+  Result.Quit := TWMPENH.Quit;
+  Result.Modify := TWMPENH.Modify;
+  Result.Config := TWMPENH.Config;
+end;
+
+class function TWMPENH.Init(const Module: PPlugin): Integer; cdecl;
+var
+  k: Integer;
+begin
+  TWMPENH.ffrm.Init();
+  for k := 0 to Length(TWMPENH.fbss) - 1 do begin
+    TWMPENH.fbss[k].Init(ftBass, btOctave, gtDb);
+  end;
+  for k := 0 to Length(TWMPENH.ftrb) - 1 do begin
+    TWMPENH.ftrb[k].Init(ftTreble, btOctave, gtDb);
+  end;
+  Result := 0;
+end;
+
+class procedure TWMPENH.Quit(const Module: PPlugin); cdecl;
+var
+  k: Integer;
+begin
+  for k := 0 to Length(TWMPENH.fbss) - 1 do begin
+    TWMPENH.fbss[k].Done();
+  end;
+  for k := 0 to Length(TWMPENH.ftrb) - 1 do begin
+    TWMPENH.ftrb[k].Done();
+  end;
+  TWMPENH.ffrm.Done();
+end;
+
+class function TWMPENH.Modify(const Module: PPlugin; const Data: Pointer; const Samples: LongWord; const Bits: LongWord; const Channels: LongWord; const Rates: LongWord): Integer; cdecl;
+var
+  k: LongWord;
+  x: LongWord;
+begin
+  if (TWMPENH.ffrm.Info.Enabled) then begin
+    TWMPENH.fdsp.Init(Data, Bits, Rates, Samples, Channels);
+    for x := 0 to Samples - 1 do begin
+      for k := 0 to Channels - 1 do begin
+        TWMPENH.fbss[k].Amp := TWMPENH.ffrm.Info.Bass.Amp;
+        TWMPENH.fbss[k].Freq := TWMPENH.ffrm.Info.Bass.Freq;
+        TWMPENH.fbss[k].Width := TWMPENH.ffrm.Info.Bass.Width;
+        TWMPENH.fbss[k].Rate := Rates;
+        TWMPENH.fdsp.Buffer[x, k] := TWMPENH.fbss[k].Process(TWMPENH.fdsp.Buffer[x, k]);
+        TWMPENH.ftrb[k].Amp := TWMPENH.ffrm.Info.Treble.Amp;
+        TWMPENH.ftrb[k].Freq := TWMPENH.ffrm.Info.Treble.Freq;
+        TWMPENH.ftrb[k].Width := TWMPENH.ffrm.Info.Treble.Width;
+        TWMPENH.ftrb[k].Rate := Rates;
+        TWMPENH.fdsp.Buffer[x, k] := TWMPENH.ftrb[k].Process(TWMPENH.fdsp.Buffer[x, k]);
+      end;
+    end;
+    TWMPENH.fdsp.Done();
+  end;
+  Result := Samples;
+end;
+
+class procedure TWMPENH.Config(const Module: PPlugin); cdecl;
+begin
+  TWMPENH.ffrm.Show();
+end;
+
+begin
+end.
---
+++ WMP/Enhancer/WMPFRM.pas
@@ -0,0 +1,331 @@
+unit
+  WMPFRM;
+
+interface
+
+uses
+//  WMPDCL,
+  Forms,
+  Controls,
+  ComCtrls,
+  StdCtrls;
+
+type
+  PFilter = ^TFilter;
+  TFilter = record
+    var Amp: Double;
+    var Freq: Double;
+    var Width: Double;
+  end;
+
+type
+  PInfo = ^TInfo;
+  TInfo = record
+    var Enabled: Boolean;
+    var Bass: TFilter;
+    var Treble: TFilter;
+  end;
+
+type
+  PWMPFRM = ^TWMPFRM;
+  TWMPFRM = record
+  private
+    var finfo: TInfo;
+    function getInfo(): TInfo;
+    function getForm(): TForm;
+    procedure Create();
+    procedure Destroy();
+    procedure FormShow(Sender: TObject);
+    procedure FormHide(Sender: TObject);
+    procedure TrackBarLoad(Sender: TObject);
+    procedure TrackBarSave(Sender: TObject);
+  public
+    procedure Init();
+    procedure Done();
+    procedure Show();
+    procedure Hide();
+    property Info: TInfo read getInfo;
+  end;
+
+implementation
+
+uses
+  SysUtils,
+  Interfaces;
+
+procedure TWMPFRM.Init();
+begin
+  Application.Initialize();
+  self.Create();
+  self.finfo.Enabled := True;
+  self.finfo.Bass.Amp := 5.0;
+  self.finfo.Bass.Freq := 160.0;
+  self.finfo.Bass.Width := 2.0;
+  self.finfo.Treble.Amp := 15.0;
+  self.finfo.Treble.Freq := 4500.0;
+  self.finfo.Treble.Width := 2.0;
+end;
+
+procedure TWMPFRM.Done();
+begin
+  self.finfo.Enabled := False;
+  self.finfo.Bass.Amp := 0.0;
+  self.finfo.Bass.Freq := 0.0;
+  self.finfo.Bass.Width := 0.0;
+  self.finfo.Treble.Amp := 0.0;
+  self.finfo.Treble.Freq := 0.0;
+  self.finfo.Treble.Width := 0.0;
+  self.Destroy();
+  Application.Terminate();
+end;
+
+procedure TWMPFRM.Show();
+begin
+  self.getForm().Show();
+end;
+
+procedure TWMPFRM.Hide();
+begin
+  self.getForm().Hide();
+end;
+
+function TWMPFRM.getInfo(): TInfo;
+begin
+  Result := self.finfo;
+end;
+
+function TWMPFRM.getForm(): TForm;
+var
+  i: Integer;
+begin
+  Result := nil;
+  for i := 0 to Application.ComponentCount - 1 do begin
+    if (Application.Components[i] is TForm) then begin
+      Result := (Application.Components[i] as TForm);
+    end;
+  end;
+end;
+
+procedure TWMPFRM.Create();
+begin
+  with (TForm.Create(Application)) do begin
+    Font.Size := 8;
+    Caption := 'Enhancer';
+    Top := 120;
+    Left := 215;
+    Width := 450;
+    Height := 250;
+    BorderIcons := [biSystemMenu];
+    BorderStyle := bsSingle;
+    Position := poMainFormCenter;
+    ShowHint := True;
+    Parent := nil;
+    OnShow := self.FormShow;
+    OnHide := self.FormHide;
+  end;
+  with (TTrackBar.Create(Application)) do begin
+    Font.Size := 8;
+    Orientation := trHorizontal;
+    Tag := 10;
+    Top := 0;
+    Left := 0;
+    Width := 450;
+    Height := 20;
+    Min := -200;
+    Max := +200;
+    Enabled := True;
+    ShowHint := True;
+    TickMarks := tmBoth;
+    TickStyle := tsNone;
+    Parent := self.getForm();
+    OnChange := self.TrackBarSave;
+  end;
+  with (TTrackBar.Create(Application)) do begin
+    Font.Size := 8;
+    Orientation := trHorizontal;
+    Tag := 11;
+    Top := 25;
+    Left := 0;
+    Width := 450;
+    Height := 20;
+    Min := 5;
+    Max := 50;
+    Enabled := True;
+    ShowHint := True;
+    TickMarks := tmBoth;
+    TickStyle := tsNone;
+    Parent := self.getForm();
+    OnChange := self.TrackBarSave;
+  end;
+  with (TTrackBar.Create(Application)) do begin
+    Font.Size := 8;
+    Orientation := trHorizontal;
+    Tag := 12;
+    Top := 50;
+    Left := 0;
+    Width := 450;
+    Height := 20;
+    Min := 50;
+    Max := 500;
+    Enabled := True;
+    ShowHint := True;
+    TickMarks := tmBoth;
+    TickStyle := tsNone;
+    Parent := self.getForm();
+    OnChange := self.TrackBarSave;
+  end;
+  with (TTrackBar.Create(Application)) do begin
+    Font.Size := 8;
+    Orientation := trHorizontal;
+    Tag := 20;
+    Top := 100;
+    Left := 0;
+    Width := 450;
+    Height := 20;
+    Min := -200;
+    Max := +200;
+    Enabled := True;
+    ShowHint := True;
+    TickMarks := tmBoth;
+    TickStyle := tsNone;
+    Parent := self.getForm();
+    OnChange := self.TrackBarSave;
+  end;
+  with (TTrackBar.Create(Application)) do begin
+    Font.Size := 8;
+    Orientation := trHorizontal;
+    Tag := 21;
+    Top := 125;
+    Left := 0;
+    Width := 450;
+    Height := 20;
+    Min := 5;
+    Max := 150;
+    Enabled := True;
+    ShowHint := True;
+    TickMarks := tmBoth;
+    TickStyle := tsNone;
+    Parent := self.getForm();
+    OnChange := self.TrackBarSave;
+  end;
+  with (TTrackBar.Create(Application)) do begin
+    Font.Size := 8;
+    Orientation := trHorizontal;
+    Tag := 22;
+    Top := 150;
+    Left := 0;
+    Width := 450;
+    Height := 20;
+    Min := 50;
+    Max := 500;
+    Enabled := True;
+    ShowHint := True;
+    TickMarks := tmBoth;
+    TickStyle := tsNone;
+    Parent := self.getForm();
+    OnChange := self.TrackBarSave;
+  end;
+end;
+
+procedure TWMPFRM.Destroy();
+begin
+  self.getForm().Close();
+  self.getForm().Destroy();
+end;
+
+procedure TWMPFRM.FormShow(Sender: TObject);
+var
+  i: Integer;
+begin
+  if (Sender is TForm) then begin
+    with (Sender as TForm) do begin
+      for i := 0 to ControlCount - 1 do begin
+        if (Controls[i] is TTrackBar) then begin
+          self.TrackBarLoad(Controls[i]);
+        end;
+      end;
+    end;
+  end;
+end;
+
+procedure TWMPFRM.FormHide(Sender: TObject);
+var
+  i: Integer;
+begin
+  if (Sender is TForm) then begin
+    with (Sender as TForm) do begin
+      for i := 0 to ControlCount - 1 do begin
+        if (Controls[i] is TTrackBar) then begin
+          self.TrackBarSave(Controls[i]);
+        end;
+      end;
+    end;
+  end;
+end;
+
+procedure TWMPFRM.TrackBarLoad(Sender: TObject);
+begin
+  if (Sender is TTrackBar) then begin
+    with (Sender as TTrackBar) do begin
+      if (Tag = 10) then begin
+        Position := Round(self.finfo.Bass.Amp * 10.0);
+        Hint := Format('Amp: %f', [self.finfo.Bass.Amp]);
+      end;
+      if (Tag = 11) then begin
+        Position := Round(self.finfo.Bass.Freq / 10.0);
+        Hint := Format('Freq: %f Hz', [self.finfo.Bass.Freq]);
+      end;
+      if (Tag = 12) then begin
+        Position := Round(self.finfo.Bass.Width * 100.0);
+        Hint := Format('Width: %f', [self.finfo.Bass.Width]);
+      end;
+      if (Tag = 20) then begin
+        Position := Round(self.finfo.Treble.Amp * 10.0);
+        Hint := Format('Amp: %f', [self.finfo.Treble.Amp]);
+      end;
+      if (Tag = 21) then begin
+        Position := Round(self.finfo.Treble.Freq / 100.0);
+        Hint := Format('Freq: %f Hz', [self.finfo.Treble.Freq]);
+      end;
+      if (Tag = 22) then begin
+        Position := Round(self.finfo.Treble.Width * 100.0);
+        Hint := Format('Width: %f', [self.finfo.Treble.Width]);
+      end;
+    end;
+  end;
+end;
+
+procedure TWMPFRM.TrackBarSave(Sender: TObject);
+begin
+  if (Sender is TTrackBar) then begin
+    with (Sender as TTrackBar) do begin
+      if (Tag = 10) then begin
+        self.finfo.Bass.Amp := Position / 10.0;
+        Hint := Format('Amp: %f', [self.finfo.Bass.Amp]);
+      end;
+      if (Tag = 11) then begin
+        self.finfo.Bass.Freq := Position * 10.0;
+        Hint := Format('Freq: %f Hz', [self.finfo.Bass.Freq]);
+      end;
+      if (Tag = 12) then begin
+        self.finfo.Bass.Width := Position / 100.0;
+        Hint := Format('Width: %f', [self.finfo.Bass.Width]);
+      end;
+      if (Tag = 20) then begin
+        self.finfo.Treble.Amp := Position / 10.0;
+        Hint := Format('Amp: %f', [self.finfo.Treble.Amp]);
+      end;
+      if (Tag = 21) then begin
+        self.finfo.Treble.Freq := Position * 100.0;
+        Hint := Format('Freq: %f Hz', [self.finfo.Treble.Freq]);
+      end;
+      if (Tag = 22) then begin
+        self.finfo.Treble.Width := Position / 100.0;
+        Hint := Format('Width: %f', [self.finfo.Treble.Width]);
+      end;
+    end;
+  end;
+end;
+
+begin
+end.
