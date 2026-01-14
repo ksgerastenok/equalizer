2f3172f6ccf15fb891ed7efbf7b0338bf33a886d
Konstantin Gerastenok <frankgover@rambler.ru>
2026-01-14 21:12:50 +0300

    Add files via upload

--- QMP/QMPTRB.pas
+++ QMP/QMPTRB.pas
@@ -43,7 +43,7 @@ begin
     TQMPTRB.fbqf[k].Init(ptZDF, ftTreble, btSlope, gtDb);
     TQMPTRB.fbqf[k].Amp := 12.0;
     TQMPTRB.fbqf[k].Freq := 3500.0;
-    TQMPTRB.fbqf[k].Width := 0.75;
+    TQMPTRB.fbqf[k].Width := 0.5;
   end;
   Result := 1;
 end;
