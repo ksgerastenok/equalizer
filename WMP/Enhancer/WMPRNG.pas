5c6d6d1f289a47acf72565ece56a278318c78555
Konstantin Gerastenok <frankgover@rambler.ru>
2026-05-20 21:45:36 +0300

    Add files via upload

--- WMP/Enhancer/WMPRNG.pas
+++ WMP/Enhancer/WMPRNG.pas
@@ -13,6 +13,7 @@ type
     var fsqr: Double;
     var favg: Double;
     var famp: Double;
+    var fval: Double;
     function getGain(): TGain;
     function getBand(): TBand;
     function getFilter(): TFilter;
@@ -25,8 +26,6 @@ type
     procedure setRate(const Value: Double);
     function getWidth(): Double;
     procedure setWidth(const Value: Double);
-    function calcAmp(): Double;
-    function calcValue(): Double;
     procedure addSample(const Value: Double);
   public
     procedure Init(const Transform: TTransform; const Filter: TFilter; const Band: TBand; const Gain: TGain);
@@ -77,29 +76,14 @@ begin
   Result := self.fbqf.Transform;
 end;
 
-function TWMPRNG.calcAmp(): Double;
-begin
-  case (self.fbqf.Gain) of
-    gtDb: begin
-      Result := Power(10.0, self.famp / 20.0);
-    end;
-    gtAmp: begin
-      Result := self.famp;
-    end;
-    else begin
-      Result := 0.0;
-    end;
-  end;
-end;
-
 function TWMPRNG.getAmp(): Double;
 begin
   case (self.fbqf.Gain) of
     gtDb: begin
-      Result := Log10(self.calcValue()) * 20.0;
+      Result := Log10(self.fval) * 20.0;
     end;
     gtAmp: begin
-      Result := self.calcValue();
+      Result := self.fval;
     end;
     else begin
       Result := 0.0;
@@ -109,7 +93,17 @@ end;
 
 procedure TWMPRNG.setAmp(const Value: Double);
 begin
-  self.famp := Value;
+  case (self.fbqf.Gain) of
+    gtDb: begin
+      self.famp := Power(10.0, Value / 20.0);
+    end;
+    gtAmp: begin
+      self.famp := Value;
+    end;
+    else begin
+      self.famp := 0.0;
+    end;
+  end;
 end;
 
 function TWMPRNG.getFreq(): Double;
@@ -142,26 +136,22 @@ begin
   self.fbqf.Width := Value;
 end;
 
-function TWMPRNG.calcValue(): Double;
-begin
-  Result := Min(Max(1.0 / self.calcAmp(), 1.0 / (self.favg + 3.0 * Sqrt(self.fsqr - Sqr(self.favg)))), 1.0 * self.calcAmp());
-end;
-
 procedure TWMPRNG.addSample(const Value: Double);
 begin
-  if (self.calcValue() * Abs(Value) < 1.0) then begin
+  if (self.fval * Abs(Value) < 1.0) then begin
     self.fsqr := self.fsqr - (self.fsqr - Sqr(Value)) / (5.0 * self.fbqf.Rate);
     self.favg := self.favg - (self.favg - Abs(Value)) / (5.0 * self.fbqf.Rate);
-  end                                      else begin
+  end                               else begin
     self.fsqr := self.fsqr - (self.fsqr - Sqr(Value)) / (0.5 * self.fbqf.Rate);
     self.favg := self.favg - (self.favg - Abs(Value)) / (0.5 * self.fbqf.Rate);
   end;
+  self.fval := Min(Max(1.0 / self.famp, 1.0 / (self.favg + 3.0 * Sqrt(self.fsqr - Sqr(self.favg)))), 1.0 * self.famp);
 end;
 
 function TWMPRNG.Process(const Value: Double): Double;
 begin
   self.addSample(self.fbqf.Process(Value));
-  Result := self.calcValue() * Value;
+  Result := self.fval * Value;
 end;
 
 begin
