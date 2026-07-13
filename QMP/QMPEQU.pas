3c54165794b7cca9d048b83c66f1ef8a1394b62a
Konstantin Gerastenok <frankgover@rambler.ru>
2026-07-13 22:41:00 +0300

    Add files via upload

--- QMP/QMPEQU.pas
+++ QMP/QMPEQU.pas
@@ -46,11 +46,11 @@ var
 begin
   for k := 0 to Length(TQMPEQU.fequ) - 1 do begin
     for i := 0 to Length(TQMPEQU.fequ[k]) - 1 do begin
-      TQMPEQU.fequ[k, i].Init(ttRBJ, ftEqu, btOctave, gtDb);
+      TQMPEQU.fequ[k, i].Init(ttZLB, ftEqu, btOctave, gtDb);
     end;
   end;
   for k := 0 to Length(TQMPEQU.frng) - 1 do begin
-    TQMPEQU.frng[k].Init(ttRBJ, ftBand, btOctave, gtDb);
+    TQMPEQU.frng[k].Init(ttZLB, ftBand, btOctave, gtDb);
   end;
   Result := 1;
 end;
