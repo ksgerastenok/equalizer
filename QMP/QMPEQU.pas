4e1fee996734d6475ebac53bb9ca3430aece5b2f
Konstantin Gerastenok <frankgover@rambler.ru>
2026-08-11 23:04:10 +0300

    Add files via upload

--- QMP/QMPEQU.pas
+++ QMP/QMPEQU.pas
@@ -88,8 +88,8 @@ begin
     end;
     for k := 0 to Length(TQMPEQU.frng) - 1 do begin
       TQMPEQU.frng[k].Amp := 20.0;
-      TQMPEQU.frng[k].Freq := 640.0;
-      TQMPEQU.frng[k].Width := 10.0;
+      TQMPEQU.frng[k].Freq := 320.0;
+      TQMPEQU.frng[k].Width := 8.0;
       TQMPEQU.frng[k].Rate := Data.Rates;
     end;
     TQMPEQU.fdsp.Init(Data);
